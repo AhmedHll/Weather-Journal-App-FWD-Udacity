eafa9a1bae9470cc4b1fe3e7e61c0ce59a99e868
AhmedHall <omdadesginer@gmail.com>
2022-02-09 19:58:04 +0200

    Test

--- server.js
+++ server.js
@@ -1,44 +1,11 @@
-// Setup empty JS object to act as endpoint for all routes
-const projectData = {};
+doA( function(){
+    doB();
 
-// Require Express to run server and routes
-const express = require('express');
+    doC( function(){
+        doD();
+    } )
 
-// Start up an instance of app
-const app = express();
+    doE();
+} );
 
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
-const bodyParser = require('body-Parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// Cors for cross origin allowance
-const cors = require('cors');
-app.use(cors());
-
-// Initialize the main project folder
-app.use(express.static('website'));
-
-// get route
-app.get("/getData", (req, res)=>{
-    res.send(projectData)
-});
-
-// post new data 
-app.post("/savingData", (req, res)=>{
-const data=req.body
-projectData.temp = data.main.temp
-projectData.name = data.name
-projectData.feelings = data.feelings
-projectData.weather = data.weather[0].main
-projectData.date = data.date
-
-res.status(200).json(data)
-});
-
-// Setup Server
-const port = 5000;
-app.listen(port, ()=>{
-    console.log(`server is life on ${port}`);
-})
+doF();
